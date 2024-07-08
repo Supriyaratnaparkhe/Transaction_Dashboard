50a3a7e3db06c881581557e503eeaac18bdf90b3
Supriyaratnaparkhe <91657954+Supriyaratnaparkhe@users.noreply.github.com>
2024-07-08 20:12:30 +0530

    Update PieChart.jsx

--- Frontend/src/Components/PieChart/PieChart.jsx
+++ Frontend/src/Components/PieChart/PieChart.jsx
@@ -29,7 +29,7 @@ const PieChartComponent = ({ month }) => {
   }, [month]);
 
   return (
-    <div>
+    <div style={{marginTop:"30px",width:"70%"}}>
       <h2>
         Pie Chart -{" "}
         {new Date(0, month - 1).toLocaleString("default", { month: "long" })}
